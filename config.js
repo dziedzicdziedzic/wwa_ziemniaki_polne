3b2beb676718b0882609a0bf8cdc3153fe4b5edf
gtluszcz <gtluszcz@gmail.com>
2018-04-20 23:08:09 +0200

    untack config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-export default class Config{
-    constructor(){
-        this.api_key = "eyJjbGllbnRJZCI6ImE0MWY1YjJhLThlODctNGI4Yi1iNmZlLTc0Y2M3NjM3MjBkN yJ9.ogVV_a9RUOMa1OWFZOTmgTkdk-U37vTliDCBUQ1YySU=";
-        this.user_agent = "hackaton2017 (Client-Id 656cbe47-b17d-46c2-bae1-3222c8777d5b) Platform";
-        this.accept = "application/vnd.allegro.public.v1+json";
-    }
-}
